a807fa093a9d22f260b64e3ac5f87cf90ae237f5
msszczep <mitchells@opinionlab.com>
2017-01-27 17:12:37 -0600

    Added basic infrastructure files: It's a start.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fasttest-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject fasttest-clj "0.1.0-SNAPSHOT"
+  :description "fasttext-clj: A Clojure clone of Fasttext"
+  :url "http://github.com/msszczep/fasttext-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/test.check "0.9.0"]])
